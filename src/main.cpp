285613b8eb230651d4ad43214c0fdf02070f9b7b
jeagerb00b <sarfarasyoonas@gmail.com>
2026-01-02 09:49:59 +0530

    wedge robot code - sarfaras
    change the parameter values accordingly and also the joy coords and the constrains

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,59 @@
-#include <Arduino.h>
+#define MAX_PWM 255
+//from here for the left side
+#define L_RPWM 25 
+#define L_LPWM 26
+#define L_REN 27
+#define L_LEN 14
+//from here for the right side
+#define R_RPWM 32
+#define R_LPWM 33
+#define R_REN 12
+#define R_LEN 13
 
-// put function declarations here:
-int myFunction(int, int);
+void setup(){
+  pinMode(L_RPWM, OUTPUT);
+  pinMode(L_LPWM, OUTPUT);
+  pinMode(L_REN, OUTPUT);
+  pinMode(L_LEN,OUTPUT);
+  pinMode(R_RPWM, OUTPUT);
+  pinMode(R_LPWM, OUTPUT);
+  pinMode(R_REN, OUTPUT);
+  pinMode(R_LEN, OUTPUT);
+  
+  digitalWrite(L_REN,HIGH);
+  digitalWrite(L_LEN,HIGH);
+  digitalWrite(R_REN,HIGH);
+  digitalWrite(R_LEN,HIGH);
+}
+void loop(){
+  int joyX = analogRead(34);//turn
+  int joyY = anmalogRead(35);//forward
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  joyX = map(joyX, 0, 4095, -255, 255);//set the coordinate maps for x joystic i have given an avg value
+  joyY = map(joyY, 0, 4095, -255, 255);
+
+  int left = joyY + joyX;
+  int right = joyY - joyX;
+
+  left = constrain(left, -255, 255);
+  right = constrain(right, -255, 255);
+
+  drivelBT2(L_RPWM, L_LPWM, left);
+  drivelBT2(R_RPWM, R_LPWM, right);  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void drivelBT2(int rpwm, int lpwm, speed){
+  if(speed > 0){
+    analogWrite(rpwm, speed);
+    analogWrite(lpwm, 0);
+  }
+  else if(speed < 0){
+    analogWrite(rpwm, 0);
+    analogWrite(lpwm, -speed);
+  }
+  else{
+    analogWrite(rpwm, 0);
+    analogWrite(lpwm, 0);
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
